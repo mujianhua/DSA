c60ef3cd3ccdaf34bdb075ef08c736d48d0de33a
mujianhua <jianhua_mu@163.com>
2022-02-15 06:11:32 +0000

    camke test

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int, char**) {
+  std::cout << "haha\n"; 
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  std::cout << "Hello, world!\n";
+  std::cout << "haha\n"; 
+}
